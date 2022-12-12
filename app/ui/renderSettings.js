1551b0873922a3b79f5a6ecca501b03830148e5f
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-12-12 13:29:24 +0200

    Filter UI fix

--- app/ui/renderSettings.js
+++ app/ui/renderSettings.js
@@ -153,28 +153,31 @@ const renderChatZone = () => {
   return elt('input', {type: 'button', name:"chatZone", value: "Set Chat Zone", className: "advanced_settings_button"});
 };
 
-const renderLanguageType = ({game, whitelist, whitelistLanguage, filterType, atMouse}) => {
-  let languages = [`eng`, `spa`, `spa_old`, `por`, `fra`, `deu`, `ita`, `chi_sim`, `chi_tra`, `kor`, `rus`];
+const renderFilterType = ({game, whitelist, filterType, atMouse}) => {
 
   if(game != `Dragonflight` && game != `Vanilla`) {
     atMouse = true;
   }
 
-  const langContainer = elt('select', {name: `whitelistLanguage`, className: `whitelistLanguage` , disabled: !whitelist}, ...languages.map( language => elt(`option`, {selected: whitelistLanguage == language}, language)));
   const atMouseContainer = elt(`input`, {name: `atMouse`, type:`checkbox`, checked: atMouse, className: `atMouse`, disabled: !whitelist || (game != `Dragonflight` && game != `Vanilla`)});
   const modeContainer =  elt(`select`, {name: `filterType`, className: `filterType`, disabled: !whitelist},
     elt(`option`, {selected: filterType == `whitelist`}, `whitelist`),
     elt(`option`, {selected: filterType == `blacklist`}, `blacklist`)
   );
 
-  return elt(`div`, null, `Mode: `, modeContainer, `Language: `, langContainer, `At Mouse:`, atMouseContainer);
+  return elt(`div`, null, `Filter Mode: `, modeContainer, `Loot win at mouse:`, atMouseContainer);
 }
 
-const renderWhitelist = ({game, whitelist, whitelistWords, whitelistLanguage, filterType}) => {
+const renderWhitelist = ({game, whitelist, whitelistWords, whitelistLanguage}) => {
+  let languages = [`eng`, `spa`, `spa_old`, `por`, `fra`, `deu`, `ita`, `chi_sim`, `chi_tra`, `kor`, `rus`];
+
+  const langContainer = elt('select', {name: `whitelistLanguage`, className: `whitelistLanguage` , disabled: !whitelist}, ...languages.map( language => elt(`option`, {selected: whitelistLanguage == language}, language)));
+
   let disabled = !whitelist;
   let checked = whitelist;
 
   return elt('div', null,
+  langContainer,
   elt('input', {type: 'text', name:"whitelistWords", placeholder: `e.g. Glacial Salmon, Pygmy Suckerfish`, className: "whitelist_input", value: whitelistWords, disabled}),
   elt('input', {type: 'checkbox', name: "whitelist", checked}))
 };
@@ -273,11 +276,11 @@ return elt(
       { className: "settings_section" },
       wrapInLabel("",
         renderWhitelist(config),
-        `The bot will loot only items in the whitelist. Before using, turn off AutoLoot in the game and set UI Scale to default. The names of the items must be exactly the same as in the game, separated by comma. `
+        `The bot will loot only items in the whitelist. Before using, turn off AutoLoot in the game and set UI Scale to default. The names of the items must be exactly the same as in the game, separated by comma. If it's the first time you using a language from the list, wait until the bot downloads the tesseract data for your language. `
       ),
       wrapInLabel("",
-        renderLanguageType(config),
-        `If it's the first time you using a language from the list, wait until the bot downloads the tesseract data for your language. The mode will decide whether to pick or to ignore the item.`
+        renderFilterType(config),
+        `Filter Mode will decide whether to pick or to ignore items in the list. Loot window at mouse will tell the bot whether it should check the loot window at mouse or the default loot window at the left side of the screen.`
       ),
       wrapInLabel(elt('span', null, "Loot all ", elt('span', {style: `color:#4DDF3F; font-weight: bold`}, `Uncommon `), `and `, elt(`span`, {style: `color: #015CB4; font-weight: bold`}, `Rare `), `and `, elt('span', {style: `color:#950c95; font-weight: bold`}, `Epic `), `items:`), renderWhiteListGreenBlue(config), `If you use whitelist, you can check this option to loot every green, blue and purple items in addition to the items in the whitelist.`)
     ),
