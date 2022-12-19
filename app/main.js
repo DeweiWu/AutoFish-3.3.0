5b7864ceaf1b154b31f4cc3fc58854cc825b0ddf
olesgeras <26541518+olesgeras@users.noreply.github.com>
2022-12-19 16:03:31 +0200

    capture protection off

--- app/main.js
+++ app/main.js
@@ -102,7 +102,7 @@ const createWindow = async () => {
   });
 
   win.loadFile("./app/index.html");
-  win.setContentProtection(true);
+
   win.on("closed", () => {
     if (process.platform === "darwin") {
       return false;
