091a1966677243f0d9ed4f0136bd0dd4bd6f47d5
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-06-29 11:50:30 +0300

    notification position fix for all games

--- app/bot/createBot.js
+++ app/bot/createBot.js
@@ -104,11 +104,11 @@ const createBot = (game, { config, settings }, winSwitch, tmBot, winNum, state) 
 
   const notificationZone = createNotificationZone({
     getDataFrom,
-    zone: Zone.from(screenSize).toRel({
-      x: 0.44,
-      y: 0.12,
-      width: 0.11,
-      height: 0.07,
+    zone: Zone.from({
+      x: Math.round((screenSize.width / 2) - (screenSize.width * config.notificationPos.width)),
+      y: Math.round(screenSize.height * config.notificationPos.y),
+      width: Math.round((screenSize.width * config.notificationPos.width) * 2),
+      height: Math.round(screenSize.height * config.notificationPos.height)
     }),
   });
 
