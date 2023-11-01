06b32deb83cb47c30f95faa8ac789221a1ef7163
jsbots <26541518+olesgeras@users.noreply.github.com>
2023-11-01 12:33:03 +0200

    fix: sound detection warning

--- app/main.js
+++ app/main.js
@@ -385,7 +385,7 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SO
   });
 
   ipcMain.on("sound-warn", () => {
-    return showWarning(win, `Turn off Music and Ambient Sounds in the game, leave only Sound Effects. Your volume should be normal. Try to find a place secluded from the sounds made by other players to avoid false detections.`);
+    return showWarning(win, `Turn off Music and Ambient Sounds in the game, leave only Sound Effects. Your volume should be normal. Try to find a place secluded from the sounds made by other players to avoid false detections.\n\nThe feature is experimental and might not work with some audio devices, in that case you need to switch to another device (e.g. you are using headphones and sound detection doesn't work, then plug in speakers and test again).`);
   });
 
   let settWin;
