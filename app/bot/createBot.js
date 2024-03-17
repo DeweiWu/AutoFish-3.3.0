318bd0c10a7c681768d13f72111ae91a63076f00
jsbots <26541518+olesgeras@users.noreply.github.com>
2024-03-17 22:58:51 +0200

    fix: initial check for errors

--- app/bot/createBot.js
+++ app/bot/createBot.js
@@ -637,7 +637,7 @@ if(lootWindowPatch.exitButton) {
 
     if (state.status == "initial") {
       await sleep(250);
-      if (!settings.autoTh && !config.ignorePreliminary && await notificationZone.check("error")) {
+      if (!config.ignorePreliminary && await notificationZone.check("error")) {
         throw new Error(`Game error notification occured on casting fishing.`);
       } else {
         state.status = "working";
