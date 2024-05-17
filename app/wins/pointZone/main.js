2c6945c3193eed08c45e093b325d0179af9d47e0
jsbots <26541518+olesgeras@users.noreply.github.com>
2024-05-17 12:48:00 +0300

    fix: more dynamic info box in pointZone

--- app/wins/pointZone/main.js
+++ app/wins/pointZone/main.js
@@ -2,16 +2,15 @@ const { BrowserWindow, ipcMain } = require("electron");
 
 const nutJS = require("@nut-tree/nut-js");
 
+let winClosed = false;
+
+const showColorPixel = async (scale) => {
+  let pos = await nutJS.mouse.getPosition();
+  let color = await(await nutJS.screen.grabRegion(new nutJS.Region(pos.x, pos.y, 1, 1))).toRGB();
+  return {x: pos.x, y: pos.y, r: color.data[0], g: color.data[1], b: color.data[2], scale};
+}
+
 const createMouseCoordsEvent = (resolve, win, botWin, scaleFactor) => {
-
-  ipcMain.handle('get-pixel-color', async (event, data) => {
-    data.x = data.x * scaleFactor;
-    data.y = data.y * scaleFactor;
-
-    let color = await(await nutJS.screen.grabRegion(new nutJS.Region(data.x, data.y, 1, 1))).toRGB();
-    return {r: color.data[0], g: color.data[1], b: color.data[2], scale: scaleFactor};
-  })
-
   ipcMain.on('mouse-coords', async (event, data) => {
     if(data) {
     data.x = data.x * scaleFactor;
@@ -19,7 +18,6 @@ const createMouseCoordsEvent = (resolve, win, botWin, scaleFactor) => {
 
     let color = await(await nutJS.screen.grabRegion(new nutJS.Region(data.x, data.y, 1, 1))).toRGB();
     data.color = {r: color.data[0], g: color.data[1], b: color.data[2]};
-
     resolve(data);
     } else {
     resolve();
@@ -31,13 +29,12 @@ const createMouseCoordsEvent = (resolve, win, botWin, scaleFactor) => {
   })
 }
 
-
-
 const createPointZone = async (botWin, scaleFactor) => {
   let win = new BrowserWindow({
     fullscreen: true,
     frame: false,
     transparent: true,
+    resizable: false,
     webPreferences: {
       contextIsolation: false,
       nodeIntegration: true,
@@ -46,10 +43,23 @@ const createPointZone = async (botWin, scaleFactor) => {
 
   win.loadFile("./app/wins/pointZone/index.html");
 
-  win.once("ready-to-show", () => {
+  win.once("ready-to-show", async () => {
     //win.openDevTools({mode: `detach`})
+
+    const showInfoBox = async () => {
+      if(win.isDestroyed()) {
+        return;
+      }
+      let data = await showColorPixel(scaleFactor);
+      win.webContents.send('show-info-box', data);
+      setTimeout(showInfoBox);
+    }
+
+    await showInfoBox()
+
     win.show();
   });
+
 
   return new Promise(function(resolve, reject) {
     createMouseCoordsEvent(resolve, win, botWin, scaleFactor);
--- app/wins/pointZone/renderer.js
+++ app/wins/pointZone/renderer.js
@@ -1,39 +1,28 @@
 const { ipcRenderer } = require('electron');
 
 let previous = null;
-let done = true;
 
-document.addEventListener('mousemove', (event) => {
+ipcRenderer.on('show-info-box', (event, {x, y, r, g, b, scale}) => {
+  if(previous) {
+    previous.remove();
+  }
 
-  if(!done) return;
+  const node = document.createElement('div');
+  previous = node;
 
-  let x = event.clientX;
-  let y = event.clientY;
-  done = false;
-  ipcRenderer.invoke('get-pixel-color', {x, y}).then(({r, g, b, scale}) => {
-    if(previous) {
-      previous.remove();
-    }
-
-    const node = document.createElement('div');
-    previous = node;
-
-    node.style =
-    `
-    position: absolute;
-    border-radius: 25px;
-    left: ${x + 10}px;
-    top: ${y - 40}px;
-    width: ${25}px;
-    height: ${25}px;
-    border: 2px solid black;
-    background-color: rgb(${r}, ${g}, ${b});
-    `
-    document.body.append(node);
-    done = true;
-  })
+  node.style =
+  `
+  position: absolute;
+  border-radius: 25px;
+  left: ${Math.floor(x  / scale) + 10}px;
+  top: ${Math.floor(y  / scale) - 40}px;
+  width: ${25}px;
+  height: ${25}px;
+  border: 2px solid black;
+  background-color: rgb(${r}, ${g}, ${b});
+  `
+  document.body.append(node);
 })
-
 
 document.addEventListener('mousedown', (event) => {
   if(event.button !== 0) {
