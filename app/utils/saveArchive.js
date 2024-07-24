75b4f750a4cfbc96583eafad10f99a5622ce1034
jsbots <26541518+olesgeras@users.noreply.github.com>
2024-07-24 13:40:35 +0300

    fix: saveArchive for af3

--- app/utils/saveArchive.js
+++ app/utils/saveArchive.js
@@ -4,13 +4,13 @@ const path = require('path');
 const archiver = require('archiver');
 const extract = require('extract-zip');
 
-const configPath = process.env.NODE_ENV == `dev` ? './config/' : '${configPath}';
+const configPath = process.env.NODE_ENV == `dev` ? './../config/' : `../../app.asar.unpacked/app/config/`;
+const configPathApp =  process.env.NODE_ENV == `dev` ? '../' : `../../app.asar.unpacked/app/`;
 
 function saveArchive(log) {
   // Open the save dialog
   dialog.showSaveDialog({
     title: 'Select Destination',
-    defaultPath: path.join(__dirname, '../../../app.asar.unpacked/app/config'),
     buttonLabel: 'Save',
     filters: [
       { name: 'Zip Files', extensions: ['zip'] }
@@ -38,7 +38,7 @@ function saveArchive(log) {
       archive.pipe(output);
 
       // Append files and folders from the /config directory to the archive
-      const configDir = path.join(__dirname, '../../../app.asar.unpacked/app/config');
+      const configDir = path.join(__dirname, configPath);
       archive.directory(configDir, 'config');
 
       // Finalize the archive (complete the file)
@@ -64,14 +64,11 @@ function loadArchive(log) {
         const filePath = file.filePaths[0];
         const fileName = path.basename(filePath, '.zip');
 
-        // Path to the /config directory
-        const configDir = path.join(__dirname, '../../../app.asar.unpacked/app');
-
         // Remove the existing /config directory
-        fs.rmSync(configDir, { recursive: true, force: true });
+        fs.rmSync(configPath, { recursive: true, force: true });
 
-        // Extract the selected archive to the /config directory
-        extract(filePath, { dir: path.join(__dirname, '../../../app.asar.unpacked/app') }).then(() => {
+        // Extract the selected archive to the /app directory
+        extract(filePath, { dir: path.join(__dirname, configPathApp) }).then(() => {
           log.ok(`Config loaded successfully.`);
           resolve();
         }).catch(err => {
