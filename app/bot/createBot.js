d1db38174b2c085bd0c1688f12624a20af5c050b
jsbots <26541518+olesgeras@users.noreply.github.com>
2025-01-24 17:29:30 +0200

    fix: allow hoverMouse for streamMode, divide the value by 2 by default

--- app/bot/createBot.js
+++ app/bot/createBot.js
@@ -2012,12 +2012,12 @@ if(lootWindowPatch.exitButton) {
   findBobber.maxAttempts = config.maxAttempts;
 
   const hoverMouse = async (chance = (config.likeHumanHover / 100)) => {
-    if(!config.likeHumanHover || config.arduino || settings.multipleWindows || settings.afkmode || config.streamMode) {
+    if(!config.likeHumanHover || config.arduino || settings.multipleWindows || settings.afkmode) { // config.streamMode
       return;
     }
 
     let cPos = mouse.getPos();
-    if(Math.random() < chance) {
+    if(Math.random() < (chance / 2)) {
       await moveTo({pos: cPos});
       return true;
     }
